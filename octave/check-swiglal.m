lscsoft/lalsuite-archive

swiglal: type conversions for strings
- Specialisations of swiglal_from and swiglal_as_val which
  pass char* strings by *value* (i.e. by copying) to/from
  native scripting language strings. Uses 'flags' whether
  or not to use LAL memory allocation routines.

- Added test code for Octave and Python

lal/swig/octave/check-swiglal.m
@@ -53,6 +53,26 @@ catch
   exit(1);
 end_try_catch
 
+## check string conversions
+try
+  strs = {"a", "bc", "def"};
+  sv = XLALCreateStringVector(strs{:});
+  assert(sv.length == 3);
+  assert(all(strcmp(sv.data, strs)));
+  strs{1} = "ghijk";
+  sv.data_setel(0, strs{1});
+  strs{end+1} = "lmnopq";
+  XLALAppendString2Vector(sv, strs{4});
+  for i = 1:sv.length
+    assert(strcmp(sv.data_getel(i-1), strs{i}));
+  endfor
+  XLALDestroyStringVector(sv);
+  LALCheckMemoryLeaks();
+catch
+  msg("FAILED string conversions");
+  exit(1);
+end_try_catch
+
 ## passed all tests!
 msg("================");
 msg("PASSED all tests");

lal/swig/python/check-swiglal.py
@@ -49,6 +49,24 @@ except:
     msg("FAILED complex number conversions")
     exit(1)
 
+# check string conversions
+try:
+    strs = ["a", "bc", "def"]
+    sv = XLALCreateStringVector(*strs)
+    assert(sv.length == 3)
+    assert(all(sv.data == strs))
+    strs[0] = "ghijk"
+    sv.data_setel(0, strs[0])
+    strs.append("lmnopq")
+    XLALAppendString2Vector(sv, strs[3])
+    for i in range(0, sv.length):
+        assert(sv.data_getel(i) == strs[i])
+    XLALDestroyStringVector(sv)
+    LALCheckMemoryLeaks()
+except:
+    msg("FAILED string conversions")
+    exit(1)
+
 # passed all tests!
 msg("================")
 msg("PASSED all tests")

kwwette/swiglal

octave/check-swiglal.m
@@ -33,6 +33,22 @@ assert(XLALCOMPLEX16Mul(complex(10, -7), complex(30, 17)) == complex(419, -40));
 assert(XLALCOMPLEX16Div(complex(111.75, -120.50), complex(5, 12)) == complex(-5.25, -11.5));
 msg("passed complex number conversions");
 
+## check string conversions
+strs = {"a", "bc", "def"};
+sv = XLALCreateStringVector(strs{:});
+assert(sv.length == 3);
+assert(all(strcmp(sv.data, strs)));
+strs{1} = "ghijk";
+sv.data_setel(0, strs{1});
+strs{end+1} = "lmnopq";
+XLALAppendString2Vector(sv, strs{4});
+assert(sv.length == 4);
+for i = 1:4
+  assert(strcmp(sv.data_getel(i-1), strs{i}));
+endfor
+XLALDestroyStringVector(sv);
+msg("passed string conversions");
+
 ## passed all tests!
 msg("================");
 msg("PASSED all tests");

python/check-swiglal.py
@@ -40,6 +40,21 @@ assert(XLALCOMPLEX16Mul(complex(10, -7), complex(30, 17)) == complex(419, -40))
 assert(XLALCOMPLEX16Div(complex(111.75, -120.50), complex(5, 12)) == complex(-5.25, -11.5))
 msg("passed complex number conversions")
 
+# check string conversions
+strs = ["a", "bc", "def"]
+sv = XLALCreateStringVector(*strs)
+assert(sv.length == 3)
+assert((sv.data == strs).all())
+strs[0] = "ghijk"
+sv.data_setel(0, strs[0])
+strs.append("lmnopq")
+XLALAppendString2Vector(sv, strs[3])
+assert(sv.length == 4)
+for i in range(0,4):
+    assert(sv.data_getel(i) == strs[i])
+XLALDestroyStringVector(sv)
+msg("passed string conversions")
+
 # passed all tests!
 msg("================")
 msg("PASSED all tests")